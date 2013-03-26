5f5ca516364021437c18960289421267bf554f98
Ryan Townsend <ryan@ryantownsend.co.uk>
2013-03-26 19:00:18 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PingMe::Application.config.secret_token = '930b1ea42b4d1c9bf568934affd4389ae8365b61e2f3e14af39df87de716df24a644e1082743b027c7c4c2024fc132c94f58ea7baa5d1ac41d8d8ad1743889ca'
